a5790c531a1646b0be879889222d54176af644c8
anselmo157 <anselmoparente@gmail.com>
2021-05-05 21:57:29 -0300

    Modificada rota inicial

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,16 +1,31 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:movedor/screens/sign_in/sign_in_screen.dart';
 import 'package:movedor/screens/splash/components/body.dart';
 import 'package:movedor/size_config.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   static String routeName = "/splash";
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 0), () => Navigator.pushNamed(context, SignInScreen.routeName));
+  }
+
   @override
   Widget build(BuildContext context) {
     // You have to call it on your starting screen
     SizeConfig().init(context);
     return Scaffold(
       backgroundColor: Color.fromRGBO(250, 250, 250, 1),
-      body: Body(),
+      body: Container(),
     );
   }
 }
