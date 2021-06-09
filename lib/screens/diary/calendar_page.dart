878198cecaee2aad3a4e4c695992f050d4d63e5b
Samiris Albuquerque <samiris.albuquerque@gmail.com>
2021-06-09 12:47:11 -0300

    Adicionando a mensagem motivacional quando uma atividade não é realizada

--- lib/screens/diary/calendar_page.dart
+++ lib/screens/diary/calendar_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:movedor/components/rounded_icon_btn.dart';
 import 'package:movedor/constants.dart';
@@ -16,6 +17,34 @@ class _CalendarPageState extends State<CalendarPage> {
   MainController controller = MainController();
   Size mediaSize;
   String aux;
+  List<String> motivationalList = ['Não desanime! Enfrente cada dia e uma meta por vez!',
+  "Sem problema! Mantenha o foco e não desanime!",
+  "Não esqueça: uma rotina de exercícios pode nos ajudar a lidar melhor com a dor!",
+  "O seu maior aliado nesse momento é a atividade física, não desista!",
+  "Organize a sua rotina para o exercício faça parte dela!",
+  "O mais importante nos exercícios físicos é praticar sempre!",
+  "Você é o seu melhor aliado! Não desista!",
+  "O repouso não vai melhor a sua dor. Viver em movimento sim!",
+  "Inicie por atividades simples e progrida aos poucos, até ganhar mais confiança!",
+  "Não desista! Você é capaz!",
+  "O melhor exercício é aquele que você gosta!",
+  "Hoje não deu, mas amanhã esperamos você para se exercitar!",
+  "Você não está sozinho! Conte conosco para continuar!",
+  "Caso você tenha feito algum tipo de exercício que não o ajudou, não significa que o exercício não funciona pra você! Tente outro tipo de exercício.",
+  "Você não precisa começar pelo movimento mais desafiador! Você precisa fazer!",
+  "Um passo de cada vez! Não desista!",
+  "Dedique um tempo para cuidar da sua saúde!",
+  "O melhor investimento para a sua vida é cuidar de si mesmo!",
+  "Não desista, você pode mais!",
+  "A persistência é o melhor caminho para o êxito!",
+  "O exercício cura!",
+  "Valorize as suas pequenas vitórias!",
+  "Não desanime! O exercício físico é o melhor aliado para quem tem dor lombar!",
+  "Cada dia é uma nova chance de realizar os seus objetivos! Não desista!",
+  "Não deixe que a sua dor lhe defina!",
+  "Estamos aqui dando suporte para você continuar!",
+  "O exercício físico é importante para a sua saúde, não deixe de realiza-lo!"];
+
 
   @override
   void initState() {
@@ -62,22 +91,68 @@ class _CalendarPageState extends State<CalendarPage> {
               CustomElevatedButton(
                 mediaSize: mediaSize,
                 text: 'Não tive tempo',
-                onPressed: () => Navigator.of(context).pop(),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _motivationalDialog();
+                },
               ),
               CustomElevatedButton(
                 mediaSize: mediaSize,
                 text: 'Não me senti motivado',
-                onPressed: () => Navigator.of(context).pop(),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _motivationalDialog();
+                },
               ),
               CustomElevatedButton(
                 mediaSize: mediaSize,
                 text: 'Minha dor aumentou após o último exercício',
-                onPressed: () => Navigator.of(context).pop(),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _motivationalDialog();
+                },
               ),
               CustomElevatedButton(
                 mediaSize: mediaSize,
                 text: 'Estava com dor no momento da realização',
-                onPressed: () => Navigator.of(context).pop(),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _motivationalDialog();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _motivationalDialog() {
+    var rng = new Random();
+    String frase = motivationalList[rng.nextInt(26)];
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          insetPadding: EdgeInsets.symmetric(horizontal: 25),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50.0))),
+          title: Text(
+            'Não desista!'
+                .toUpperCase(), 
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'MontserratRegular',
+              color: Color(0xff36a9b0),
+              fontSize: mediaSize.width * 0.05,
+            ),
+          ),
+          content: Wrap(
+            children: [
+              Text(
+                frase,
               ),
             ],
           ),
