83921f6b14606838a2d7b4f2676036118f0a3628
Thiago Leal Menezes <thiago.pixelado@gmail.com>
2021-07-01 18:32:02 -0300

    Changed statistic_page to new design.

--- lib/screens/diary/statistic_page.dart
+++ lib/screens/diary/statistic_page.dart
@@ -1,131 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-class StatisticPage extends StatefulWidget {
-  @override
-  _StatisticPageState createState() => _StatisticPageState();
-}
-
-class _StatisticPageState extends State<StatisticPage> {
-  Size mediaSize;
-
+class StatisticPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    mediaSize = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: Color(0xFFF5F6F9),
-      body: SingleChildScrollView(
-        child: Column(
+    Size mediaSize = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFF5F6F9),
+        body: Column(
           children: [
             Container(
-              margin: EdgeInsets.only(top: mediaSize.height * 0.05),
-              child: Text(
-                "Atividade",
-                style: TextStyle(
-                  color: Color(0xff36a9b0),
-                  fontSize: 24,
-                ),
+              margin: EdgeInsets.only(
+                top: mediaSize.height * 0.05,
+                left: mediaSize.height * 0.03,
               ),
-            ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 20),
-                width: mediaSize.width * 0.9,
-                height: mediaSize.height * 0.4,
-                child: Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32)),
-                  color: const Color(0xff020227),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 50, right: 25),
-                    child: BarChart(
-                      BarChartData(
-                        alignment: BarChartAlignment.center,
-                        maxY: 60,
-                        minY: 0,
-                        groupsSpace: 25,
-                        barTouchData: BarTouchData(enabled: true),
-                        titlesData: FlTitlesData(
-                          topTitles: BarTitles.getTopBottomTitles(),
-                        ),
-                        barGroups: BarData.barData
-                            .map((data) => BarChartGroupData(
-                                  x: data.id,
-                                  barsSpace: 4,
-                                  barRods: [
-                                    BarChartRodData(
-                                        y: data.y,
-                                        width: 22,
-                                        colors: [data.color],
-                                        borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(6),
-                                          topRight: Radius.circular(6),
-                                        ))
-                                  ],
-                                ))
-                            .toList(),
-                      ),
-                    ),
-                  ),
+              width: double.maxFinite,
+              child: Text(
+                'ESTATÍSTICAS',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.only(top: 30),
+              margin: EdgeInsets.only(
+                top: mediaSize.height * 0.02,
+                left: mediaSize.height * 0.03,
+              ),
+              width: double.maxFinite,
               child: Text(
-                "Percepção de Esforço",
+                'Exercícios aeróbios',
+                textAlign: TextAlign.start,
                 style: TextStyle(
-                  color: Color(0xff36a9b0),
-                  fontSize: 24,
+                  color: Colors.black,
+                  fontSize: 20,
                 ),
               ),
             ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 20),
-                width: mediaSize.width * 0.9,
-                height: mediaSize.height * 0.4,
-                child: Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32)),
-                  color: const Color(0xff020227),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 50, right: 25),
-                    child: BarChart(
-                      BarChartData(
-                        alignment: BarChartAlignment.center,
-                        maxY: 10,
-                        minY: 0,
-                        groupsSpace: 25,
-                        barTouchData: BarTouchData(enabled: true),
-                        titlesData: FlTitlesData(
-                          topTitles: BarTitles.getTopBottomTitles(),
+            barChart(mediaSize, BarData.barData),
+            Container(
+              margin: EdgeInsets.only(
+                top: mediaSize.height * 0.02,
+                left: mediaSize.height * 0.03,
+              ),
+              width: double.maxFinite,
+              child: Text(
+                'Exercícios de fortalecimento',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            barChart(mediaSize, BarData.barDataEffort),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget barChart(Size _mediaSize, List<Data> _barData) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        width: _mediaSize.width * 0.9,
+        height: _mediaSize.height * 0.3,
+        child: Card(
+          elevation: 4,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+          child: Padding(
+            padding: EdgeInsets.only(top: 10, right: 25, left: 25),
+            child: BarChart(
+              BarChartData(
+                alignment: BarChartAlignment.center,
+                maxY: 40,
+                minY: 0,
+                groupsSpace: _mediaSize.width * 0.05,
+                barTouchData: BarTouchData(
+                  enabled: true,
+                  touchTooltipData: BarTouchTooltipData(
+                    tooltipBgColor: Colors.transparent,
+                    tooltipPadding: const EdgeInsets.all(0),
+                    tooltipMargin: 0,
+                    getTooltipItem: (
+                      BarChartGroupData group,
+                      int groupIndex,
+                      BarChartRodData rod,
+                      int rodIndex,
+                    ) {
+                      return BarTooltipItem(
+                        (rod.y.round() - 1).toString(),
+                        TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
                         ),
-                        barGroups: BarData.barDataEffort
-                            .map((data) => BarChartGroupData(
-                                  x: data.id,
-                                  barsSpace: 4,
-                                  barRods: [
-                                    BarChartRodData(
-                                        y: data.y,
-                                        width: 22,
-                                        colors: [data.color],
-                                        borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(6),
-                                          topRight: Radius.circular(6),
-                                        ))
-                                  ],
-                                ))
-                            .toList(),
-                      ),
-                    ),
+                      );
+                    },
                   ),
                 ),
+                backgroundColor: Colors.white,
+                titlesData: FlTitlesData(
+                  leftTitles: SideTitles(showTitles: false),
+                  bottomTitles: BarTitles.getTopBottomTitles(),
+                ),
+                borderData: FlBorderData(show: false),
+                barGroups: _barData
+                    .map((data) => BarChartGroupData(
+                          x: data.id,
+                          barsSpace: 4,
+                          showingTooltipIndicators: [0],
+                          barRods: [
+                            BarChartRodData(
+                                y: data.y + 1,
+                                width: 22,
+                                colors: [Color(0xff36a9b0), Color(0xffa9d6c2)],
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(6),
+                                  topRight: Radius.circular(6),
+                                ))
+                          ],
+                        ))
+                    .toList(),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -151,45 +155,45 @@ class BarData {
 
   static List<Data> barData = [
     Data(
+      name: 'DOM',
       id: 0,
-      name: 'Domingo',
-      y: 24,
+      y: 0,
       color: Color(0xff19bfff),
     ),
     Data(
-      name: 'Segunda',
+      name: 'SEG',
       id: 1,
-      y: 20,
+      y: 15,
       color: Color(0xffff4d94),
     ),
     Data(
-      name: 'Terça',
+      name: 'TER',
       id: 2,
-      y: 18,
+      y: 20,
       color: Color(0xff2bdb90),
     ),
     Data(
-      name: 'Quarta',
+      name: 'QUA',
       id: 3,
-      y: 25,
+      y: 0,
       color: Color(0xffffdd80),
     ),
     Data(
-      name: 'Quinta',
+      name: 'QUI',
       id: 4,
-      y: 34,
+      y: 15,
       color: Color(0xff2bdb90),
     ),
     Data(
-      name: 'Sexta',
+      name: 'SEX',
       id: 5,
-      y: 16,
+      y: 30,
       color: Color(0xffffdd80),
     ),
     Data(
-      name: 'Sábado',
+      name: 'SÁB',
       id: 6,
-      y: 58,
+      y: 0,
       color: Color(0xffff4d94),
     ),
   ];
@@ -198,43 +202,43 @@ class BarData {
     Data(
       id: 0,
       name: 'Domingo',
-      y: 2,
+      y: 0,
       color: Color(0xff19bfff),
     ),
     Data(
       name: 'Segunda',
       id: 1,
-      y: 4,
+      y: 10,
       color: Color(0xffff4d94),
     ),
     Data(
       name: 'Terça',
       id: 2,
-      y: 5,
+      y: 0,
       color: Color(0xff2bdb90),
     ),
     Data(
       name: 'Quarta',
       id: 3,
-      y: 3,
+      y: 10,
       color: Color(0xffffdd80),
     ),
     Data(
       name: 'Quinta',
       id: 4,
-      y: 8,
+      y: 0,
       color: Color(0xff2bdb90),
     ),
     Data(
       name: 'Sexta',
       id: 5,
-      y: 9,
+      y: 0,
       color: Color(0xffffdd80),
     ),
     Data(
       name: 'Sábado',
       id: 6,
-      y: 8,
+      y: 0,
       color: Color(0xffff4d94),
     ),
   ];
@@ -243,8 +247,11 @@ class BarData {
 class BarTitles {
   static SideTitles getTopBottomTitles() => SideTitles(
         showTitles: true,
-        getTextStyles: (value) =>
-            const TextStyle(color: Colors.white, fontSize: 10),
+        getTextStyles: (value) => const TextStyle(
+          color: Colors.black,
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),
         margin: 10,
         getTitles: (double id) => BarData.barData
             .firstWhere((element) => element.id == id.toInt())
